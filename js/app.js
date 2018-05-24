9b8b3e7d368fcc652f08e6fafc885bb0ba31dfae
Mia Ćaćić <cacic.miaa@gmail.com>
2018-05-24 23:00:23 +0200

    Try responsive

--- js/app.js
+++ js/app.js
@@ -8,21 +8,22 @@ let secondCard = 0;
 let openCards = [];
 let openList = [];
 let preA = [];
-const stars = document.querySelector('.stars'); 
+const stars = document.querySelector('.stars');
 const deck = document.querySelector('.deck');
 deck.innerHTML = '';
 const countMoves = document.querySelector('.moves');
 let counter = 0;
 countMoves.textContent = "0";
 const restartB = document.querySelector('.restart');
-let seconds = 0, minutes = 0; 
+let seconds = 0,
+    minutes = 0;
 let secs, mins;
 let clearTime;
 const modal = document.getElementById('modal');
 const closeMe = document.querySelector('.close-btn');
 const playAgain = document.querySelector('.play-again');
 const contemplate = document.querySelector('.contemplate');
-let stats=document.querySelector('.stats');
+let stats = document.querySelector('.stats');
 
 
 
@@ -35,7 +36,8 @@ let stats=document.querySelector('.stats');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -50,32 +52,36 @@ function shuffle(array) {
 startGame();
 
 function startGame() {
-	shuffle(cards);
-	const fragment = document.createDocumentFragment();
-	for ( let i = 0; i < cards.length; i++) {
-		let cardEl = cards[i];
-		cardEl.classList.remove('show', 'open', 'match', 'disabled');
-		fragment.appendChild(cardEl);
-	}
-	deck.appendChild(fragment);
-	startWatch();
-	deck.addEventListener('click', flipMe);
-	
+    shuffle(cards);
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < cards.length; i++) {
+        let cardEl = cards[i];
+        cardEl.classList.remove('show', 'open', 'match', 'disabled');
+        fragment.appendChild(cardEl);
+    }
+    deck.appendChild(fragment);
+    startWatch();
+    deck.addEventListener('click', flipMe);
+
 }
 
- function startWatch( ) { 
-	if ( seconds === 60 ) { seconds = 0; minutes++; } 
-	mins =( minutes < 10 ) ? ( `0 ${minutes} :` ) : ( `${minutes} : `);
-	if ( minutes === 60 ) { restartGame(); } 
-	secs = ( seconds < 10 ) ? ( `0 ${seconds}` ) : ( seconds ); 
-	const timer = document.querySelector('.timer'); 
-	timer.innerHTML = ` ${mins} ${secs}`; 
-	seconds++; 
-	clearTime = setTimeout( "startWatch( )", 1000 ); 
-	}; 
-	
+function startWatch() {
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+    mins = (minutes < 10) ? (`0 ${minutes} :`) : (`${minutes} : `);
+    if (minutes === 60) {
+        restartGame();
+    }
+    secs = (seconds < 10) ? (`0 ${seconds}`) : (seconds);
+    const timer = document.querySelector('.timer');
+    timer.innerHTML = ` ${mins} ${secs}`;
+    seconds++;
+    clearTime = setTimeout("startWatch( )", 1000);
+};
 
-	
+
 
 
 /*
@@ -92,113 +98,118 @@ function startGame() {
 
 
 function moveCounter() {
-	
-	countMoves.textContent = counter.toString();
-	
-	
-	if (counter === 9 ) { 
-		changeStar();
-		}
-	if (counter === 13) {
-		changeStar();
-		}
-	if (counter === 17) {
-		changeStar();
-		}
-		function changeStar() {
-			let starList = Array.from(document.querySelectorAll('.fa-star'));
-			let last = starList.length;
-		starList[last-1].classList.replace('fa-star','fa-star-o');
-		}
-	}
+
+    countMoves.textContent = counter.toString();
 
 
- 
-function result() {
-	let classOfFirst = firstCard.firstElementChild.className;
-	let classOfSecond = secondCard.firstElementChild.className;
-		if (classOfFirst == classOfSecond) {
-		for (let i=0; i < openCards.length; i++) {
-			openCards[i].classList.add('match','disabled');
-					 }
-		preA = openCards;
-		openList = openList.concat(preA); 
-		
-	}
-	else { firstCard.classList.remove('show', 'open','disabled');
-		secondCard.classList.remove('show', 'open','disabled');
-				
-	}
-openCards.splice(0,2);	
-preA.splice(0,2);
-firstCard=0;
-secondCard=0;
-counter++;
-moveCounter();
-if (openList.length == 16) {
-	clearTimeout(clearTime);
-	stats.innerHTML = ` Time: ${mins}${secs}. Moves: ${counter}.`;
-	modal.style.display = 'block';
-	
+    if (counter === 9) {
+        changeStar();
+    }
+    if (counter === 13) {
+        changeStar();
+    }
+    if (counter === 17) {
+        changeStar();
+    }
+
+    function changeStar() {
+        let starList = Array.from(document.querySelectorAll('.fa-star'));
+        let last = starList.length;
+        starList[last - 1].classList.replace('fa-star', 'fa-star-o');
+    }
 }
-deck.addEventListener('click', flipMe);
+
+
+
+function result() {
+    let classOfFirst = firstCard.firstElementChild.className;
+    let classOfSecond = secondCard.firstElementChild.className;
+    if (classOfFirst == classOfSecond) {
+        for (let i = 0; i < openCards.length; i++) {
+            openCards[i].classList.add('match', 'disabled');
+        }
+        preA = openCards;
+        openList = openList.concat(preA);
+
+    } else {
+        firstCard.classList.remove('show', 'open', 'disabled');
+        secondCard.classList.remove('show', 'open', 'disabled');
+
+    }
+    openCards.splice(0, 2);
+    preA.splice(0, 2);
+    firstCard = 0;
+    secondCard = 0;
+    counter++;
+    moveCounter();
+    if (openList.length == 16) {
+        clearTimeout(clearTime);
+        stats.innerHTML = ` Time: ${mins}${secs}. Moves: ${counter}.`;
+        modal.style.display = 'block';
+
+    }
+    deck.addEventListener('click', flipMe);
 
 };
- 
-function setTimer() { 
-	const fTime = setTimeout(result, 750);
 
-	
+function setTimer() {
+    const fTime = setTimeout(result, 750);
+
+
 }
+
 function showMe() {
-	
-	setTimer();
+
+    setTimer();
 }
- 
- 
+
+
 function flipMe(e) {
-	if (openCards.length<2) {
-	 if (!e.target.classList.contains('card')) return; 
-	 	
- if (!firstCard) {
-	 firstCard = e.target;
-	 firstCard.classList.add('show', 'open', 'disabled');
-	 openCards.push(firstCard);	 
- } 
-	else { 
-	secondCard = e.target;
-	secondCard.classList.add('show','open','disabled');
-	openCards.push(secondCard);
-	showMe();
-	}
-	}
-	};
+    if (openCards.length < 2) {
+        if (!e.target.classList.contains('card')) return;
+
+        if (!firstCard) {
+            firstCard = e.target;
+            firstCard.classList.add('show', 'open', 'disabled');
+            openCards.push(firstCard);
+        } else {
+            secondCard = e.target;
+            secondCard.classList.add('show', 'open', 'disabled');
+            openCards.push(secondCard);
+            showMe();
+        }
+    }
+};
 
 restartB.addEventListener('click', restartGame);
+
 function restartGame() {
-	let starsBack = Array.from(document.querySelectorAll('.fa-star-o'));
-	for (i=0; i < starsBack.length; i++) {
-		starsBack[i].classList.replace('fa-star-o','fa-star');
-		}
-	counter = 0;
-	countMoves.textContent = "0";
-	openList.splice(0,16);
-	clearTimeout(clearTime);
-	minutes = 0, seconds = 0;
-	mins = '00 :';
-	secs = '00';
-	startGame();
+    let starsBack = Array.from(document.querySelectorAll('.fa-star-o'));
+    for (i = 0; i < starsBack.length; i++) {
+        starsBack[i].classList.replace('fa-star-o', 'fa-star');
+    }
+    counter = 0;
+    countMoves.textContent = "0";
+    openList.splice(0, 16);
+    clearTimeout(clearTime);
+    minutes = 0, seconds = 0;
+    mins = '00 :';
+    secs = '00';
+    startGame();
 }
 closeMe.addEventListener('click', closeModal);
+
 function closeModal() {
-	modal.style.display = 'none';
+    modal.style.display = 'none';
 }
 playAgain.addEventListener('click', seriously);
+
 function seriously() {
-	restartGame();
-	modal.style.display = 'none';
+    restartGame();
+    modal.style.display = 'none';
 }
 contemplate.addEventListener('click', doThat);
+
 function doThat() {
-	document.body.style.display ='none';
+    document.body.style.display = 'none';
 }
